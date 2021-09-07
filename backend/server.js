6fedf018f420dc7b55f8fdc1ae9a4996dd7915ad
kalaksanan <nankalaksa@gmail.com>
2021-09-07 18:31:27 +0700

    setup backend and frontend

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const { application } = require('express')
+const express = require('express')
+const notes = require('./data/notes')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send(`API is running on port ${PORT}`)
+})
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+  const note = notes.find((n) => n._id === req.params.id)
+  res.send(note)
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`server run on port ${PORT}`))
